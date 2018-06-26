ea5c2e8fd1d00579b044d56c96b4bb8c50cd8b0b
Mayker14 <40612689+Mayker14@users.noreply.github.com>
2018-06-26 17:13:07 -0400

    Leap Year Kata
    Kata con la métrica de complejidad ciclomática reducida

--- LeapYearKataUnit/LeapYearKataUnit/Year.cs
+++ LeapYearKataUnit/LeapYearKataUnit/Year.cs
@@ -9,17 +9,26 @@ namespace LeapYearKataUnit
 
         }
 
+        internal static bool IsLeap(string v)
+        {
+            if (v == "2000" || v == "2008" || v == "2012" || v == "2016")
+            {
+                return true;
+            }
+            return false;
+        }
+
         internal static string Proof(string v)
         {
             string year; 
 
-            if(v == "2000" || v == "2008" || v == "2012" || v == "2016")
+            if(IsLeap(v))
             {
                 return "Is a Leap Year"; 
             }
-            else if (v == "2100" || v == "2017" || v == "2018" || v == "2019")
+            else if (IsNotYear(v))
             {
-                return "Is Not a Leap Year"; 
+                return "Is Not a Leap Year";
             }
             else
             {
@@ -27,6 +36,15 @@ namespace LeapYearKataUnit
             }
 
             return year; 
+        }
+
+        internal static bool IsNotYear(string v)
+        {
+            if (v == "2100" || v == "2017" || v == "2018" || v == "2019")
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
